5a7fbd205b71ca97351592045b68c6b4cd0a0333
Ruslan Afanasiev <ruslanafanasiev555@gmail.com>
2022-10-30 18:17:42 +0700

    сделал костяк приложения + начал делать модели. Всё заново =)
    Начинаю с application, далее буду создавать классы и развивать, то что написано в данном клссе

--- src/main/java/com/money/currencyconverter/CurrencyConverterApplication.java
+++ src/main/java/com/money/currencyconverter/CurrencyConverterApplication.java
@@ -3,12 +3,29 @@ package com.money.currencyconverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CurrencyConverterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConverterApplication.class, args);
+	}
+	@Bean
+	ApplicationRunner init(CurrencyRepository repository, CurrencyRateRepository rateRepository){
+		//При запуске приложения необходимо получить список актуальных валют и их курсов с сайта ЦБРФ
+		//и записать их в базу данных а также курсы (привязанные к валюте) на дату запроса.
+		DataFromXML data = XMLService.parseRates();
+		return args -> {
+			repository.save(new Currency("1", "111", "RUB", 1, "Российский рубль"));
+			for (Currency c:data.getCurrencies())
+				repository.save(c);
+			rateRepository.save(new CurrencyRate("1", null, "RUB", 1.0));
+			for (CurrencyRate cr:data.getCurrencyRates())
+				rateRepository.save(cr);
+		};
 	}
 
 }
