b76c08c42af98b8470b4792caa1aa2e38cfc5fdd
Ruslan Afanasiev <ruslanafanasiev555@gmail.com>
2022-10-30 20:21:44 +0700

    сделал костяк приложения + начал делать модели. Всё заново =)
    Начинаю с application, далее буду создавать классы и развивать, то что написано в данном клссе
    
    Основнову заложили: парсинг и открытие, продолжаю

--- src/main/java/com/money/currencyconverter/CurrencyConverterApplication.java
+++ src/main/java/com/money/currencyconverter/CurrencyConverterApplication.java
@@ -1,11 +1,18 @@
 package com.money.currencyconverter;
 
+import com.money.currencyconverter.models.CurrencyRate;
+import com.money.currencyconverter.models.DataAPI;
+import com.money.currencyconverter.repositories.CurrencyRateRepository;
+import com.money.currencyconverter.repositories.CurrencyRepository;
+import com.money.currencyconverter.services.APIService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Currency;
 
 @SpringBootApplication
 public class CurrencyConverterApplication {
@@ -17,7 +24,7 @@ public class CurrencyConverterApplication {
 	ApplicationRunner init(CurrencyRepository repository, CurrencyRateRepository rateRepository){
 		//При запуске приложения необходимо получить список актуальных валют и их курсов с сайта ЦБРФ
 		//и записать их в базу данных а также курсы (привязанные к валюте) на дату запроса.
-		DataFromXML data = XMLService.parseRates();
+		DataAPI data = APIService.parseRates();
 		return args -> {
 			repository.save(new Currency("1", "111", "RUB", 1, "Российский рубль"));
 			for (Currency c:data.getCurrencies())
